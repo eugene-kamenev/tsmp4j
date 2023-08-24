6c3ade9a8aea9b3bb127fbf71324961faa79ec40
eugene <eugene.kamenev@gmail.com>
2023-08-24 09:09:33 +0600

    fix(stats-computation): An attempt to reduce numeric errors
    * because of double usage for calculations, there is small numeric error for some cases
    * this still requires experimental proof

--- src/main/java/com/github/eugene/kamenev/tsmp4j/stats/BaseRollingWindowStatistics.java
+++ src/main/java/com/github/eugene/kamenev/tsmp4j/stats/BaseRollingWindowStatistics.java
@@ -21,6 +21,8 @@ import com.github.eugene.kamenev.tsmp4j.utils.Buffer;
 import com.github.eugene.kamenev.tsmp4j.utils.Buffer.DoubleBuffer;
 import com.github.eugene.kamenev.tsmp4j.utils.Buffer.ObjBuffer;
 import com.github.eugene.kamenev.tsmp4j.utils.Util;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 /**
  * Class computes rolling window statistics for a data stream, which is used in Matrix Profile
@@ -32,11 +34,21 @@ public class BaseRollingWindowStatistics<S extends WindowStatistic>
     private final Buffer.DoubleBuffer dataBuffer;
 
     private final Buffer.ObjBuffer<S> statsBuffer;
-    protected double currentMean = 0;
-    protected double varianceSum = 0;
-    private int dataCount = 0;
+
+    /**
+     * Usage of {@link MathContext#DECIMAL128} is required to avoid precision loss when computing
+     * stats, but still, since we are not using BigDecimal for every computation, some errors are expected.
+     */
+    private final MathContext mathContext = MathContext.DECIMAL128;
+    private long dataCount = 0;
     private long totalDataCount = 0;
     private int toSkip = 0;
+    private BigDecimal sum = BigDecimal.ZERO;
+    private BigDecimal sumSq = BigDecimal.ZERO;
+
+    protected double varianceSum = 0.0d;
+
+    private double currentMean = 0.0d;
 
     public BaseRollingWindowStatistics(int windowSize, S[] statsBuffer) {
         this.dataBuffer = new DoubleBuffer(windowSize);
@@ -51,14 +63,6 @@ public class BaseRollingWindowStatistics<S extends WindowStatistic>
     @Override
     public S apply(double dataPoint) {
         this.totalDataCount++;
-        if (this.getDataBuffer().isFull()) {
-            double oldestData = this.getDataBuffer().head();
-            double updatedMean =
-                (this.dataCount * this.currentMean - oldestData) / (this.dataCount - 1);
-            this.varianceSum -= (oldestData - this.currentMean) * (oldestData - updatedMean);
-            this.currentMean = updatedMean;
-            this.dataCount--;
-        }
 
         if (Double.isNaN(dataPoint) || Double.isInfinite(dataPoint)) {
             this.toSkip = this.windowSize();
@@ -67,18 +71,37 @@ public class BaseRollingWindowStatistics<S extends WindowStatistic>
             this.toSkip--;
         }
 
-        this.getDataBuffer().addToEnd(dataPoint);
-        this.dataCount++;
-
-        double previousMean = this.currentMean;
-        this.currentMean += (dataPoint - this.currentMean) / this.dataCount;
-        this.varianceSum += (dataPoint - previousMean) * (dataPoint - this.currentMean);
-
-        double populationVariance = 0;
-        if (this.dataCount > 1) {
-            populationVariance = this.varianceSum / this.dataCount;
+        if (this.getDataBuffer().isFull()) {
+            var oldestData = this.getDataBuffer().head();
+            var x = BigDecimal.valueOf(oldestData);
+            this.sum = this.sum.subtract(x, mathContext);
+            this.sumSq = this.sumSq.subtract(x.multiply(x, mathContext));
+            double updatedMean =
+                (this.dataCount * this.currentMean - oldestData) / (this.dataCount - 1);
+            this.varianceSum -= (oldestData - this.currentMean) * (oldestData - updatedMean);
+            this.currentMean = updatedMean;
+            this.dataCount--;
         }
-        var stat = computeStats(dataPoint, this.currentMean, computeStdDev(populationVariance),
+        this.getDataBuffer().addToEnd(dataPoint);
+        var x = BigDecimal.valueOf(dataPoint);
+        this.dataCount++;
+        var count = BigDecimal.valueOf(dataCount);
+        this.sum = this.sum.add(x);
+        this.sumSq = this.sumSq.add(x.multiply(x, mathContext));
+        var mean = this.sum.divide(count, mathContext);
+        double previousMean = this.currentMean;
+        this.currentMean = mean.doubleValue();
+        this.varianceSum += (dataPoint - previousMean) * (dataPoint - this.currentMean);
+        var populationVariance = BigDecimal.ZERO;
+        var stdDev = BigDecimal.ZERO;
+        if (this.dataCount > 1) {
+            var meanSq = mean.multiply(mean, mathContext);
+            populationVariance = sumSq.divide(count, mathContext)
+                .subtract(meanSq, mathContext);
+            stdDev = populationVariance.compareTo(BigDecimal.ZERO) >= 0 ? populationVariance.sqrt(
+                mathContext) : BigDecimal.ZERO;
+        }
+        var stat = computeStats(dataPoint, mean.doubleValue(), stdDev.doubleValue(),
             totalDataCount, this.toSkip > 0);
         this.getStatsBuffer().addToEnd(stat);
         return stat;
